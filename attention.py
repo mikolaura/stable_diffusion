06f511ddae652b1114e21ab67b370cdedcec9c91
mikolaura <mikolaura58@gmail.com>
2024-09-27 19:15:30 +0300

    vae done

--- attention.py
+++ attention.py
@@ -1,4 +1,4 @@
-import token 
+import torch 
 from torch import nn
 import torch.nn.functional as F
 import math
@@ -8,4 +8,35 @@ class SelfAttention(nn.Module):
         super().__init__()
         
         self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
-        
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x: torch.Tensor, causal_mask: torch.Tensor):
+        # x: (B, S, D)
+        input_shape = x.shape
+        batch_size, sequence_length, d_embed = input_shape
+
+        interm_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
+        q, k, v = self.in_proj(x).chunk(3, dim=-1)
+
+        q = q.view(interm_shape).transpose(1, 2)
+        k = k.view(interm_shape).transpose(1, 2)
+        v = v.view(interm_shape).transpose(1, 2)
+                
+        weight = q @ k.transpose(-1, -2)
+
+        if causal_mask is not None:
+            mask = torch.ones_like(weight, dtype=torch.bool).triu(1)
+            weight.masked_fill_(mask, -torch.inf)
+
+        weight /= math.sqrt(self.d_head)
+        weight = F.softmax(weight, dim=-1)
+
+        output = weight @ v
+        output = output.reshape(input_shape)
+        output = self.out_proj(output)
+
+        return output
+
