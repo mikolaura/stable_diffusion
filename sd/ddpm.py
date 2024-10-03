dfdec2bf2bf3504be4c5ec17e4af378d4108f785
mikolaura <mikolaura58@gmail.com>
2024-10-03 17:13:08 +0300

    Started making ddpm

---
+++ sd/ddpm.py
@@ -0,0 +1,28 @@
+import torch
+import numpy as np
+
+
+class DDPMSampler:
+
+    def __init__(
+        self,
+        generator: torch.Generator,
+        num_training_steps=1000,
+        beta_start=0.00085,
+        beta_end=0.0120,
+    ):
+        self.betas = (
+            torch.linspace(
+                beta_start**0.5, beta_end**0.5, num_training_steps, dtype=torch.float32
+            )
+            ** 2
+        )
+        self.alphas = 1 - self.betas
+        self.alpha_cumprod = torch.cumprod(self.alphas, 0)
+        self.one = torch.tensor(1.0)
+        self.generator = generator
+        self.num_training_steps = num_training_steps
+        self.timesteps = torch.from_numpy(np.arange(0, num_training_steps)[::-1].copy())
+
+    def set_inference_timesteps(self, num_inference_steps=50):
+        self.num_inference_steps = num_inference_steps
