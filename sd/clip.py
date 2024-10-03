783ad1d80174cf78d9e86da214c536f34de01407
mikolaura <mikolaura58@gmail.com>
2024-10-03 19:41:34 +0300

    maybe last2

--- sd/clip.py
+++ sd/clip.py
@@ -44,7 +44,7 @@ class CLIP(nn.Module):
     def __init__(self):
         super().__init__()
         self.embedding = CLIPEmbedding(49408, 768, 77)
-        self.layers = nn.Module([CLIPLayer(12, 768) for _ in range(12)])
+        self.layers = nn.ModuleList([CLIPLayer(12, 768) for _ in range(12)])
         self.layernorm = nn.LayerNorm(768)
 
     def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
